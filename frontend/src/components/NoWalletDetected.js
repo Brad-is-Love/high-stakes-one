1fbf3e7a026a6c0779e8277d2e2d6fd78060d474
Bradley Sandilands <bradsandilands@hotmail.com>
2023-10-22 11:53:01 +0800

    push alpha

--- frontend/src/components/NoWalletDetected.js
+++ frontend/src/components/NoWalletDetected.js
@@ -10,14 +10,6 @@ export function NoWalletDetected() {
               No wallet was detected. <br />
               Please install{" "}
               <a
-                href="https://www.coinbase.com/wallet"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Coinbase Wallet
-              </a>
-              or{" "}
-              <a
                 href="http://metamask.io"
                 target="_blank"
                 rel="noopener noreferrer"
