a54b2ca0e4aae1629581622c338e1f12be415acd
Bradley Sandilands <bradsandilands@hotmail.com>
2023-11-04 10:16:45 +1300

    updates

--- frontend/src/components/Prizes.js
+++ frontend/src/components/Prizes.js
@@ -6,35 +6,10 @@ export function Prizes({ winners, loading, latestBlock, lowestBlock, getData }) 
     <>
       <div className="row">
         <div className="col-12">
-          <h4>Last Winners</h4>
-          <div className="table-responsive">
-            <table className="table table-sm">
-              <thead>
-                <tr>
-                  <th>Date</th>
-                  <th>Winner</th>
-                  <th>Prize</th>
-                </tr>
-              </thead>
-              <tbody>
-                {winners.map((winner, index) => {
-                  return (
-                    <tr key={index}>
-                      <td><p>{winner.date}</p></td>
-                      <td><p>{winner.winner}</p></td>
-                      <td><p>{winner.amount}</p></td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-          {loading ? <p>Loading...</p> : 
-          latestBlock > lowestBlock ?
-          <button className="btn btn-primary" onClick={getData}>
-            Load More
-          </button> : <p>No more winners</p>
-          }
+            <h4>Last Prize</h4>
+            <p>
+                <strong>{lastPrize}</strong> was won by <strong>{lastWinner}</strong>
+            </p>
         </div>
       </div>
       {/* div with centered content */}
