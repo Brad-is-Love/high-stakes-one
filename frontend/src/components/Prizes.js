5131db0edff1a5c2d902db0c864cf1f69df523a1
Bradley Sandilands <bradsandilands@hotmail.com>
2023-11-17 13:41:57 +1300

    just need to move api calls into luckystaker component

--- frontend/src/components/Prizes.js
+++ frontend/src/components/Prizes.js
@@ -6,10 +6,33 @@ export function Prizes({ winners, loading, latestBlock, lowestBlock, getData }) 
     <>
       <div className="row">
         <div className="col-12">
-            <h4>Last Prize</h4>
-            <p>
-                <strong>{lastPrize}</strong> was won by <strong>{lastWinner}</strong>
-            </p>
+          <h4>Last Winners</h4>
+          <div className="table-responsive">
+            <table className="table table-sm">
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Winner</th>
+                  <th>Prize</th>
+                </tr>
+              </thead>
+              <tbody>
+                {winners.map((winner, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{winner.date}</td>
+                      <td>{winner.winner}</td>
+                      <td>{winner.amount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+
+          <button className="btn btn-primary" onClick={getData}>
+            Load More
+          </button>
         </div>
       </div>
       {/* div with centered content */}
